0b2cd3a8e33c45c51f2547536c51f89da2951cb8
Lou Broue <lobroue@z2r10p6.42lyon.fr>
2026-01-13 20:08:50 +0100

    Work from 42

--- get_next_line.c
+++ get_next_line.c
@@ -26,8 +26,8 @@ char    *get_next_line(int fd)
     read_and_stash(fd, &stash, &readed);
     if (stash == NULL)
         return (NULL);
-    
-    
+    extract_line(stash,&line);
+    // clean up stash
 
 
     return (line);
@@ -35,23 +35,53 @@ char    *get_next_line(int fd)
 void    read_and_stash(int fd, t_list **stash, int  *readed_ptr)
 {
     char    *buf;
-    buf = malloc(sizeof(char) * (BUFFER_SIZE + 1));
-    if (buf == NULL)
-        return;
+    
     while (!found_newline(*stash) && *readed_ptr != 0)
     {
-        *readed_ptr = read(fd, buf, BUFFER_SIZE);
-        buf[*readed_ptr] = '\0';
+        buf = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+    if (buf == NULL)
+        return;
+        *readed_ptr = (int)read(fd, buf, BUFFER_SIZE);
         if((*stash == NULL && *readed_ptr == 0) || *readed_ptr != 0)
-
+        {
+            free(buf);
+            return;
+        }
+        buf[*readed_ptr] = '\0';
+        add_to_stash(stash, buf, readed_ptr);
+        free(buf);
     }
 }
 
-void    add_to_stash()
+void    add_to_stash(t_list **stash, char *buf, int readed)
 {
-    
+    int i;
+    t_list  *last;
+    t_list  *new_node;
+
+    new_node = malloc(sizeof(t_list));
+    if(new_node == NULL)
+        return;
+    new_node->next = NULL;
+    new_node->content = malloc (sizeof(char) * (readed + 1));
+    if(new_node->content == NULL)
+        return;
+    i = 0;
+    while (buf[i] && i < readed)
+    {
+        new_node->content[i] = buf[i];
+        ++i;
+    }
+    new_node->content[i] = '\0';
+    if (*stash == NULL)
+    {
+        *stash = new_node;
+        return;
+    }
+    last = ft_lst_get_lest(*stash);
+    last->next = new_node;
 }
-void    extract_line()
+void    extract_line(t_list stash, char **line)
 {
     
 }
